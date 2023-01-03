d26fa71d0ed427ab84f050de6d9385af17aa5276
riswan a <113181008+Riswanrio@users.noreply.github.com>
2023-01-03 14:18:15 +0530

    Add files via upload

---
+++ lib/model/game_model.dart
@@ -0,0 +1,153 @@
+
+
+class GameModel {
+  final String name;
+  final String image;
+  final String description;
+  final double speed, health, attack;
+
+  GameModel({
+    required this.name,
+    required this.image,
+    required this.description,
+    required this.speed,
+    required this.health,
+    required this.attack,
+  });
+}
+
+final List<GameModel> heroes = [
+  GameModel(
+    name: 'Barbarian',
+    image: 'assets/images/player1.png',
+    description: '',
+    speed: 18.0,
+    health: 60.0,
+    attack: 80.0,
+  ),
+  GameModel(
+    name: 'Archer',
+    image: 'assets/images/player2.png',
+    description: '',
+    speed: 28.0,
+    health: 55.0,
+    attack: 70.0,
+  ),
+  GameModel(
+    name: 'Giant',
+    image: 'assets/images/player3.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Goblin',
+    image: 'assets/images/player4.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Wall Breaker',
+    image: 'assets/images/player5.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Balloon',
+    image: 'assets/images/player6.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Wizard',
+    image: 'assets/images/player7.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Healer',
+    image: 'assets/images/player8.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Dragon',
+    image: 'assets/images/player9.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'P.E.K.K.A',
+    image: 'assets/images/player10.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Baby Dragon',
+    image: 'assets/images/player11.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Miner',
+    image: 'assets/images/player12.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Electro Dragon',
+    image: 'assets/images/player13.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Yeti (Yetimite)',
+    image: 'assets/images/player14.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+  GameModel(
+    name: 'Dragon Rider',
+    image: 'assets/images/player15.png',
+    description: '',
+    speed: 30.0,
+    health: 90.0,
+    attack: 88.0,
+  ),
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
